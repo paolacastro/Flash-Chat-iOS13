9f011498ddb30e08031c1e08c81aff44a5ede95e
Paola Castro <paolacastropaolacastro@gmail.com>
2020-01-08 12:03:29 -0800

    added xib cells

---
+++ Flash Chat iOS13/Controllers/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Paola Castro on 12/11/19.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Paola Castro on 12/12/19.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
